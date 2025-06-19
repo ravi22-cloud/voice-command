0e23b82a7b94c426b775ca3c3e3bbeeff0d18dbe
ravi22-cloud <ravidass2402@gmail.com>
2025-06-19 19:00:16 +0530

    Update voice.py

--- voice.py
+++ voice.py
@@ -7,7 +7,6 @@ import datetime
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-engine.setProperty("rate", 120)
 
 def speak(audio):
     engine.say(audio)
